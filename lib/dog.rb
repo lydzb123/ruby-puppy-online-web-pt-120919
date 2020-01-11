8d6b52df3071b9e289cfeb7566aed24b44d10da4
Lydia B <lydiabaik@gmail.com>
2020-01-11 04:36:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
-# Add your code here
+require 'pry'
+
+class Dog
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    self.save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear_all
+    @@all = []
+    @@all
+  end
+
+  def self.print_all
+    @@all.join()
+  end
+
+
+  def save
+    @@all << self
+  end
+
+
+
+end
